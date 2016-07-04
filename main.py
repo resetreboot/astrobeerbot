a7f00839f505fd7be90c587a0955515283cd7292
José Carlos Cuevas <jose.carlos@finhubtechnologies.com>
2016-07-04 11:27:30 +0200

    Added the basic functionality, sending the APOD to the channel

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #! /usr/bin/env python
 
 import logging
@@ -6,6 +7,7 @@ import sys
 
 from bs4 import BeautifulSoup
 from telegram.ext import Updater, Handler, CommandHandler, MessageHandler
+from telegram import ChatAction
 
 from config import config
 
@@ -23,11 +25,36 @@ def start(bot, update):
 
 
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Aquí debería ir un mensaje de ayuda, pero voy algo pedo.')
+    bot.sendMessage(update.message.chat_id, text="""Astro Beer Bot v1.0:
+Para mostrar la APOD actual, escribe /apod y te la mostraré.
+                    """)
 
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+# This is were the fun begins
+def apod(bot, update):
+    apod = requests.get('http://apod.nasa.gov/apod/astropix.html')
+    if apod.status_code > 299:
+        bot.sendMessage(update.message.chat_id, text='La NASA esta como las grecas, así que no hay APOD.')
+        return
+
+    soup = BeautifulSoup(apod.text, 'html.parser')
+    img_url = 'http://apod.nasa.gov/'
+    img_url += soup.find_all('img')[0]['src']
+    meta = "Astronomy Picture of the Day"
+    for tag in soup.find_all('meta'):
+        if tag['name'] == 'keywords':
+            meta = tag['content']
+
+    print(img_url)
+    print(meta)
+
+    bot.sendMessage(update.message.chat_id, text=meta)
+    bot.sendChatAction(update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
+    bot.sendPhoto(update.message.chat_id, photo=img_url)
 
 
 def main():
@@ -43,6 +70,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("apod", apod))
 
     # log all errors
     dispatcher.add_error_handler(error)
