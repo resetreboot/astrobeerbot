c5d7dea2457c402a837898871cc2c38355641251
José Carlos Cuevas <jose.carlos@finhubtechnologies.com>
2016-07-04 15:15:12 +0200

    Added help about the new feature

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def start(bot, update):
 def help(bot, update):
     bot.sendMessage(update.message.chat_id, text="""Astro Beer Bot v1.0:
 Para mostrar la APOD actual, escribe /apod y te la mostraré.
+Con /tiempo te diré qué tal se presenta el tiempo esta misma noche.
                     """)
 
 
